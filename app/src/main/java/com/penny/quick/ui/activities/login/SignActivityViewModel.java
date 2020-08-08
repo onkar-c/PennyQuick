47e0c8d07ac4aa73310349871052ca7996d7404f
kashmira <kashmira.sakunde@gmail.com>
2020-08-08 17:35:59 +0530

    Dev_08_aug : Login and success login screen done,intro screen design changes

---
+++ app/src/main/java/com/penny/quick/ui/activities/login/SignActivityViewModel.java
@@ -0,0 +1,8 @@
+package com.penny.quick.ui.activities.login;
+
+import androidx.lifecycle.ViewModel;
+
+public class SignActivityViewModel extends ViewModel {
+
+  SignActivityViewModel(){super();}
+}
