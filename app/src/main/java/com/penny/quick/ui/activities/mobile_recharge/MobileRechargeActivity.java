533d30bed44f2e38a1fe99fa009507383df53f2c
kashmira <kashmira.sakunde@gmail.com>
2020-08-09 16:44:25 +0530

    dev/09_aug:Mobile recharge screen design

---
+++ app/src/main/java/com/penny/quick/ui/activities/mobile_recharge/MobileRechargeActivity.java
@@ -0,0 +1,15 @@
+package com.penny.quick.ui.activities.mobile_recharge;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.penny.quick.R;
+
+public class MobileRechargeActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView( R.layout.activity_mobile_recharge);
+  }
+}
