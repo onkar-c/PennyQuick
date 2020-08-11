883754ab77d044e69c68eb38aca2ff2e4d33e579
onkar-c <chopadeonkar@gmail.com>
2020-08-11 23:07:07 +0530

    transaction screen created.

---
+++ app/src/main/java/com/penny/quick/ui/activities/transaction_status/TransactionStatusActivity.java
@@ -0,0 +1,15 @@
+package com.penny.quick.ui.activities.transaction_status;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.penny.quick.R;
+import com.penny.quick.utils.ToolBarUtils;
+
+public class TransactionStatusActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_transaction_status);
+  }
+}
